a553b39ec13d52bd017fb68c6b9cd78ec838f084
Juram <gimjulam2@gmail.com>
2026-03-23 14:22:47 +0900

    counter HTML onclick

--- 02_counter/main.js
+++ 02_counter/main.js
@@ -1,14 +1,20 @@
 let count = 0;
 
-const num = document.getElementsByTagName("h1")[0];
-const plusButton = document.getElementsByTagName("button")[0];
+const num = document.getElementById("plus");
+// const num = document.querySelector("#plus");
+// const plusButton = document.getElementsByTagName("button")[0];
 
 // plusButton.addEventListener("click", () => {
 //     count++;
 //     num.innerHTML = count;
 // });
 
-plusButton.onclick=()=>{
+// plusButton.onclick=()=>{
+//     count++;
+//     num.innerHTML = count;
+// };
+
+function plus(){
     count++;
     num.innerHTML = count;
-};
+}
